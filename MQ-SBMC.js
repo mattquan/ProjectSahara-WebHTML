017c215a8f9517abafe8ffe784af67be44d9097c
nickprimamore <lancernp@gmail.com>
2016-12-08 19:03:49 -0500

    Separated fill methods for starting/ending lists. Added an "onClick" method that displays 2 points.

--- MQ-SBMC.js
+++ MQ-SBMC.js
@@ -8,17 +8,42 @@ var masterList = ["peter","joe","bob","small","yellow","room","friday","mark","r
 
 onload = init;
 function init() {
-    fillStartingAndEndingInputs();
+    fillStartingInputs();
+    fillEndingInputs();
 }
-function fillStartingAndEndingInputs() {
-    var roomList = document.getElementById('roomList');
-    //create a new option element that fills the datalist (roomList)
+function fillStartingInputs() {
+    var list1 = document.getElementById('roomList1');
+    //create a new option element that fills the datalist (roomList1)
     for (var i =0; i <masterList.length;i++) {
-        var option = document.createElement('option');
-        option.value = masterList[i];
-        roomList.appendChild(option);
+        var option1 = document.createElement('option'); 
+        option1.value = masterList[i];
+        list1.appendChild(option1);
     }
-    console.log(roomList);
-    
+    console.log(list1);  		
+}
+
+function fillEndingInputs() {
+    var roomList2 = document.getElementById('roomList2');
+    //creates a new option element that fills the datalist(roomList2)
+    for (var i =0; i <masterList.length;i++) {
+        var option2 = document.createElement('option');
+        option2.value = masterList[i];
+        roomList2.appendChild(option2);
+    }
+    console.log(roomList2);                                                               		
+}
+
+function navigateButtonClicked() {
+
+	
+	var selectedList1 = document.getElementById('StartingPosInput');
+	var startingPoint = selectedList1.value;
+
+	var selectedList2 = document.getElementById('EndingPosInput');
+	var endingPoint = selectedList2.value;
+
+	
+	alert(document.getElementById('navigationText').innerHTML = "Navigating from " + startingPoint + " to " + endingPoint + "...");
+
 }
 
