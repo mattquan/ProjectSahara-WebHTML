e9fa4eae7806dbb102e764f83b6116550c024f89
mattquan <quan.matthew1999@gmail.com>
2017-06-28 21:03:27 -0400

    to the Node.js!

---
+++ test.js
@@ -0,0 +1,18 @@
+
+
+var express = require('express');
+var app = express();
+var path = require('path');
+const _dirname = "C:/Users/Matt/Desktop/Node Sandbox";
+
+app.use(express.static(path.join(_dirname+"/Scripts")));
+app.use(express.static(path.join(_dirname+"/HTML")));
+app.use(express.static(path.join(_dirname+"/Styles")));
+
+app.listen(8080, function() {
+  console.log("Server opened...");
+});
+//so if it gets a request with nothing there, it will do this
+app.get('/' , function(req, res) {
+  res.sendFile(path.join(_dirname+'/index.html'));
+});
